265d36598a38d09227170f9788589e97c04f9a79
Kanishk <vchaganti2@horizon.csueastbay.edu>
2026-01-24 18:47:57 -0800

    cotrollers and routes for /Register and /Login

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+// Register user
+const registerUser = async (req, res) => {
+  res.send('Register route working');
+};
+
+// Login user
+const loginUser = async (req, res) => {
+  res.send('Login route working');
+};
+
+module.exports = { registerUser, loginUser };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth.routes');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use('/api/auth', authRoutes);
 // Connect to MongoDB
 connectDB();
 
